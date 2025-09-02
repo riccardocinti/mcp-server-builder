cd26425d41730babc1962adf565335300174d099
Riccardo <cintiriccardo@gmail.com>
2025-09-02 17:45:00 +0200

    Project structure implementation

---
+++ src/main/java/com/riccardocinti/mcp_server_build/mcp_server_build/McpServerBuildApplication.java
@@ -0,0 +1,13 @@
+package com.riccardocinti.mcp_server_build.mcp_server_build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpServerBuildApplication.class, args);
+	}
+
+}
