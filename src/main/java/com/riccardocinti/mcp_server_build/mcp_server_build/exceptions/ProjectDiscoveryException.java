7efb2170434b6b6307bfbbc51ff22039f1edb6a9
Riccardo <cintiriccardo@gmail.com>
2025-09-03 21:17:13 +0200

    1 Project discovery and validation step

--- src/main/java/com/riccardocinti/mcp_server_build/mcp_server_build/exceptions/ProjectDiscoveryException.java
+++ src/main/java/com/riccardocinti/mcp_server_build/mcp_server_build/exceptions/ProjectDiscoveryException.java
@@ -1,4 +1,6 @@
 package com.riccardocinti.mcp_server_build.mcp_server_build.exceptions;
 
 public class ProjectDiscoveryException extends Exception {
+    public ProjectDiscoveryException(String s) {
+    }
 }
--- src/main/java/com/riccardocinti/mcp_server_build/mcp_server_build/model/ProjectInfo.java
+++ src/main/java/com/riccardocinti/mcp_server_build/mcp_server_build/model/ProjectInfo.java
@@ -10,4 +10,52 @@ public record ProjectInfo(String projectPath,
                           BuildTool buildTool,
                           String buildFilePath,
                           List<String> additionalFiles) {
+
+    public static ProjectInfoBuilder builder() {
+        return new ProjectInfoBuilder();
+    }
+
+    public static final class ProjectInfoBuilder {
+        String projectPath;
+        String projectName;
+        String projectVersion;
+        BuildTool buildTool;
+        String buildFilePath;
+        List<String> additionalFiles;
+
+        public ProjectInfoBuilder projectPath(String projectPath) {
+            this.projectPath = projectPath;
+            return this;
+        }
+
+        public ProjectInfoBuilder projectName(String projectName) {
+            this.projectName = projectName;
+            return this;
+        }
+
+        public ProjectInfoBuilder projectVersion(String projectVersion) {
+            this.projectVersion = projectVersion;
+            return this;
+        }
+
+        public ProjectInfoBuilder buildTool(BuildTool buildTool) {
+            this.buildTool = buildTool;
+            return this;
+        }
+
+        public ProjectInfoBuilder buildFilePath(String buildFilePath) {
+            this.buildFilePath = buildFilePath;
+            return this;
+        }
+
+        public ProjectInfoBuilder additionalFiles(List<String> additionalFiles) {
+            this.additionalFiles = additionalFiles;
+            return this;
+        }
+
+        public ProjectInfo build() {
+            return new ProjectInfo(projectPath, projectName, projectVersion,
+                    buildTool, buildFilePath, additionalFiles);
+        }
+    }
 }
